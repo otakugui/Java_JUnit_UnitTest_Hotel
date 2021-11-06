00fb280ee75282c35342bd51ae2232f0b798eafc
Guilherme L. De Cicco <guilherme.cicco@users.noreply.github.com>
2021-11-06 16:23:14 -0300

    Adicionado Teste Simples para classe Room

---
+++ src/test/java/com/marcelodebittencourt/RoomTest.java
@@ -0,0 +1,22 @@
+package com.marcelodebittencourt;
+import org.junit.jupiter.api.Test;
+
+public class RoomTest {
+    
+    @Test
+    void testNewRoom(){
+        //Entrada de Dados
+        int NumberofRoom = 200;
+        String NameofRoom = "Suite Luxo";
+        double ValorPerDay = 500;
+
+        //T - Teste - Execução do Testel
+        Room RoomLuxo = Room(NumberofRoom, NameofRoom, ValorPerDay);
+
+        //R - Resultado - Validação de Resultado
+        assertEquals(NumberofRoom, RoomLuxo.getId());
+        assertEquals(NameofRoom, RoomLuxo.getName() );
+        assertEquals(ValorPerDay, RoomLuxo.getPricePerDay() );
+    }
+
+}
